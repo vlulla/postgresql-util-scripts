42bea591bdb74ee91507298f836a9623d70025d6
Vijay Lulla <vijaylulla@gmail.com>
2022-03-23 15:17:34 -0400

    dynamic query generation to find number of nulls in columns

---
+++ count-nulls-in-cols-of-table.sql
@@ -0,0 +1,20 @@
+--
+-- 2022.03.23
+--
+-- Running this query would be a *bad idea*...especially for big tables. Therefore, I just print the sql statements that could be helpful
+-- in explorations. Just copy/paste the query and edit it to meet your needs!
+--
+with q(qry) as (
+  with nullqueries(qry, tblname) as (
+    select 'COUNT(*) FILTER (WHERE ' || quote_ident(attname) || ' is null) AS ' || quote_ident(attname::text) || '_numnull', quote_ident(c.relnamespace::regnamespace::text) || '.' || quote_ident(c.relname::text)
+    from pg_attribute join pg_class c on c.oid = attrelid and c.relkind = 'r' where attnum > 0 order by c.relname, attnum
+  )
+  -- select E'EXPLAIN SELECT\n' || string_agg(qry, E', \n') || E'\nFROM ' || tblname || E';\n' from nullqueries
+  select E'SELECT\n' || string_agg(qry, E', \n') || E'\nFROM ' || tblname || E';\n' from nullqueries
+  where tblname not SIMILAR TO '"?pg_%'
+  group by tblname
+) select qry from q
+    -- where qry like '%mag_core%'
+;
+
+-- \gexec
