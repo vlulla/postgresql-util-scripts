42239eb230adba0bc8adb13ca155fd3c121820cc
Vijay Lulla <vijaylulla@gmail.com>
2023-02-22 13:36:09 -0500

    Query to generate current year dates on the day of week.
    Very useful for planning! I have found running `psql -f
    generate-current-year-calendar.sql tst` on an instance where i have
    access to psql to be an immense timesaver.

---
+++ generate-current-year-calendar.sql
@@ -0,0 +1,17 @@
+-- Generate list of dates for each of the days.
+--
+-- This is how to use it:
+--
+-- bash $ <generate-current-year-calendar.sql psql mydb
+-- bash $ <generate-current-year-calendar.sql psql -c '\pset format csv' -f - mydb
+--
+-- 2023.02.22
+--
+with y(yr) as (select extract(year from now())),
+     days(dy) as (select generate_series((yr||'-01-01')::date,(yr||'-12-31')::date,interval 'P1D') from y)
+select
+  dy::date
+  -- to_char(dy,'MON, DD') dy
+  , to_char(dy,'IW') iso_week, to_char(dy,'WW') week_number,to_char(dy,'DY') d,to_char(dy,'D') dow from days
+\crosstabview week_number d dy dow
+-- ;
