1c216f0fa7404bf8f7c22a568e407222f003812b
Vijay Lulla <vijaylulla@gmail.com>
2023-02-22 23:25:54 -0500

    Use ISO year, week and day numbers to generate standardized calendar.
    ISO 8601 day of the week, Monday(1) to Sunday(7).
    ISO 8601 week number (01-53; the first Thursday of year is in week 1).
    
    Using ISO 8601 values standardizes the date/time calculations and
    display.

--- generate-current-year-calendar.sql
+++ generate-current-year-calendar.sql
@@ -7,11 +7,12 @@
 --
 -- 2023.02.22
 --
+
 with y(yr) as (select extract(year from now())),
      days(dy) as (select generate_series((yr||'-01-01')::date,(yr||'-12-31')::date,interval 'P1D') from y)
 select
   dy::date
   -- to_char(dy,'MON, DD') dy
-  , to_char(dy,'IW') iso_week, to_char(dy,'WW') week_number,to_char(dy,'DY') d,to_char(dy,'D') dow from days
-\crosstabview week_number d dy
+  , (to_char(dy,'IYYY_IW')) iso_year_week,to_char(dy,'DY') d,to_char(dy,'ID') dow from days
+\crosstabview iso_year_week d dy dow
 -- ;
