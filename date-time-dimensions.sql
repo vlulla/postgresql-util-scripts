b98616ee871f9ca90bd87b1ffe07b3cfaf5e9bba
Vijay Lulla <vijay_lulla@sweetwater.com>
2023-08-15 14:23:12 -0400

    Date dimensions for the data warehouse

---
+++ date-time-dimensions.sql
@@ -0,0 +1,27 @@
+-- Neat idea based on https://wiki.postgresql.org/wiki/Date_and_Time_dimensions
+--
+-- For PostgreSQL
+
+select
+  datum::date as date
+ ,extract(year from datum) as year
+ ,extract(month from datum) as month
+ ,to_char(datum,'TMMonth') as MonthName
+ ,extract(day from datum) as Day
+ ,extract(doy from datum) as DayOfYear
+ ,to_char(datum,'TMDay') as WeekdayName
+ ,extract(week from datum) as CalendarWeek
+ ,to_char(datum,'yyyy.mm.dd') as FormattedDate
+ ,'Q'||to_char(datum,'Q') as Quarter
+ ,to_char(datum,'yyyy/"Q"Q') as YearQuarter
+ ,to_char(datum,'yyyy/mm') as YearMonth
+ ,to_char(datum,'iyyy/IW') as ISOYearCalendarWeek
+ ,case when extract(isodow from datum) in (6,7) then 'Weekend' else 'Weekday' end as Weekend
+ -- ISO start and end of the week of this date! -- VERY USEFUL!
+ ,datum::date + (1 - extract(isodow from datum))::integer as CWStart
+ ,datum::date + (7 - extract(isodow from datum))::integer as CWEnd
+ ,datum::date + (1 - extract(day from datum))::integer as MonthStart
+ ,(datum::date + (1 - extract(day from datum))::integer + '1 month'::interval - interval '1 day')::date as MonthEnd
+from generate_series('2010-01-01'::date,'2019-12-31'::date,interval '1 day') as df(datum)
+order by datum
+;
